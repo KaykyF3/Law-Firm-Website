bd626ff71b53100030a7470dc8467b4f638e24e4
Kayky F <107002484+KaykyF3@users.noreply.github.com>
2024-03-01 13:15:58 -0300

    commit

--- script.js
+++ script.js
@@ -10,23 +10,18 @@ window.sr = ScrollReveal({
     reset: false,
     mobile:false
 })
-
-//First Container
-sr.reveal(".sub-containers",{delay:200})
-
 sr.reveal(".first-sub-container-img,.first-text-box,.main-title-element,.second-img-box,.third-text-box,.fourth-img-box",{
     delay: 100,
     origin: 'left'
 })
 
-/*Segundo Container*/
 sr.reveal(".second-sub-container-text,.first-img-box,.second-text-box,.third-img-box,.fourth-text-box",{
     duration: 1000,
     distance: '35px',
     origin: 'right'
 })
-/*Quarto Container*/
-sr.reveal(".first-card,.second-card,.third-card",{
+
+sr.reveal(".sub-containers,.first-card,.second-card,.third-card",{
     duration: 1500,
     distance: '35px',
     delay: 200
